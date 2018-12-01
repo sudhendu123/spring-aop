ccaf17d2dd6442cd431ab9225807d7cabe3d5acb
SUDHENDU.HM@GMAIL.COM <SUDHENDU.HM@GMAIL.COM>
2018-12-01 23:22:54 +0530

    aop

---
+++ SpringAOP-Aspects-Order-Example-master/src/com/doj/aopapp/aspect/SecurityAspect.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.doj.aopapp.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Dinesh.Rajput
+ *
+ */
+@Aspect
+@Component
+public class SecurityAspect implements Ordered{
+	
+	/**
+	 * Declaring before advice for all transfer methods whose taking three arguments of any type 
+	 * of all classes in the package com.doj.aopapp.service
+	 * @param jp
+	 * @throws Throwable
+	 */
+	@Before("execution(* com.doj.aopapp.service.*.transfer(*,*,*))")
+	public void beforeAdviceForTransferMethods(JoinPoint jp) throws Throwable {
+        System.out.println("****SecurityAspect.beforeAdviceForTransferMethods() " + jp.getSignature().getName());
+    }
+
+	@Override
+	public int getOrder() {
+		return 0;
+	}
+}
